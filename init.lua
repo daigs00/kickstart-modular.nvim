4bb2c09cfb6f24c7f5c9f4ccd4659c431ed112fa
Max Daigle <maxdaigle00@gmail.com>
2025-01-22 14:35:18 -0700

    Updates

--- init.lua
+++ init.lua
@@ -105,11 +105,7 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
-require 'lualine-config'
-
-require 'pylsp-config'
-
-require 'noice-config'
+require 'configs'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/configs.lua
@@ -0,0 +1,111 @@
+-- Lualine Configuration
+-- This configuration is mainly implemented from their github repository.
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {},
+  virtual_env = function()
+    -- only show virtual env for Python
+    if vim.bo.filetype ~= 'python' then
+      return ''
+    end
+
+    local conda_env = os.getenv 'CONDA_DEFAULT_ENV'
+    local venv_path = os.getenv 'VIRTUAL_ENV'
+
+    if venv_path == nil then
+      if conda_env == nil then
+        return ''
+      else
+        return string.format('  %s (conda)', conda_env)
+      end
+    else
+      local venv_name = vim.fn.fnamemodify(venv_path, ':t')
+      return string.format('  %s (venv)', venv_name)
+    end
+  end,
+}
+
+require('noice').setup {
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+}
+
+local lspconfig = require 'lspconfig'
+lspconfig.pylsp.setup {
+  on_attach = custom_attach,
+  settings = {
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = false, executable = 'pylint' },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = true, ignore = { 'E501' } },
+        -- type checker
+        pylsp_mypy = { enabled = true },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
+      },
+    },
+  },
+  flags = {
+    debounce_text_changes = 200,
+  },
+  capabilities = capabilities,
+}
